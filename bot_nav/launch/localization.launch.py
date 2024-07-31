d538ad278c15a1a0650985085ec7734f4f6ccb48
Vinothhk <iamvinothkumar16@gmail.com>
2024-07-31 19:23:22 +0530

    map modified

--- bot_nav/launch/localization.launch.py
+++ bot_nav/launch/localization.launch.py
@@ -6,7 +6,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
 
     nav2_yaml = os.path.join(get_package_share_directory('bot_nav'), 'config','amcl.yaml')
-    map_file = os.path.join(get_package_share_directory('bot_nav'), 'maps', 'open_map.yaml')
+    map_file = os.path.join(get_package_share_directory('bot_nav'), 'maps', 'map_new.yaml')
     
     return LaunchDescription([
         Node(
